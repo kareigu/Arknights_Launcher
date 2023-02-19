89eb539df02e5ca0a97c19244b294811a201b1bc
karei <eetu.kivelae@gmail.com>
2023-02-19 02:30:54 +0200

    Add outset border on MainView buttons

--- src/views/MainView.svelte
+++ src/views/MainView.svelte
@@ -23,11 +23,11 @@
   on:mouseenter={() => (skew_to_hover = true)}
   on:mouseleave={() => (skew_to_hover = false)}
 >
-  <button class="launch-button button-text" on:click={launch}>
+  <button class="launch-button button-text button-border" on:click={launch}>
     <span class="launch-button-text">{launch_button_text}</span>
   </button>
   <button
-    class="options-button button-text"
+    class="options-button button-text button-border"
     on:click={() => CurrentView.set(View.Options)}
   >
     <span> ⚙️ </span>
@@ -47,14 +47,27 @@
     transform: skew(var(--mouse-x), var(--mouse-y));
   }
 
+  .button-border {
+    border-width: 0.15rem;
+    border-color: rgba(0, 0, 0, 0.1);
+    border-style: none outset outset none;
+  }
+
+  .button-border:hover {
+    border-width: 0.18rem;
+  }
+
+  .button-border:active {
+    border-width: 0.2rem;
+  }
+
   .launch-button {
     height: var(--button-height);
     width: var(--primary-button-width);
-    border: 0;
     position: relative;
     color: var(--ak-black);
     background-color: var(--button-white);
-    transition: background-color 80ms ease-in;
+    transition: all 80ms ease-in;
   }
 
   .launch-button:hover {
@@ -70,10 +83,9 @@
   .options-button {
     height: var(--button-height);
     width: var(--secondary-button-width);
-    border: 0;
     color: var(--ak-white);
     background-color: var(--button-blue);
-    transition: background-color 80ms ease-in;
+    transition: all 80ms ease-in;
   }
 
   .options-button:hover {
